01cc6536c5c92e6c605914c913bc8921ec970a22
meta-akshat-khandelwal <46737793+meta-akshat-khandelwal@users.noreply.github.com>
2019-02-25 17:41:38 +0530

    Update TwitterService.java

--- EAD-Assignment-4/EAD-Assignment-4/src/com/metacube/messageService/TwitterService.java
+++ EAD-Assignment-4/EAD-Assignment-4/src/com/metacube/messageService/TwitterService.java
@@ -1,7 +1,11 @@
 package com.metacube.messageService;
 
 public class TwitterService implements MessageService{
-	
+	/*
+	*sends message 
+	*@param,message
+	*@param, receiver
+	*/
 	@Override
 	public void sendMessage(String message, String receiver)
 	{
