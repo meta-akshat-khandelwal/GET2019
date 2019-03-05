0a03d571a07955a439f1efaaca9abdfae055aedd
meta-akshat-khandelwal <akshat.khandelwal@meatcube.com>
2019-03-05 14:29:01 +0530

    Assignment 6

---
+++ EAD-Assignment-6/src/main/java/com/metacube/EADAssignment6/mapper/UserMapper.java
@@ -0,0 +1,26 @@
+package com.metacube.EADAssignment6.mapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.RowMapper;
+
+import com.metacube.EADAssignment6.model.User;
+/**
+ * Maps the data extracted from database to user object
+ * @author Akshat
+ *
+ */
+public class UserMapper implements RowMapper<User> {
+
+	public User mapRow(ResultSet rs, int i) throws SQLException {
+		
+		User user =new User();
+		user.setId(rs.getInt(1));
+		user.setFirst_name(rs.getString(2));
+		user.setLast_name(rs.getString(3));
+		user.setContact_number(rs.getString(4));
+		user.setEmail(rs.getString(5));
+		return user;	
+	}
+}
