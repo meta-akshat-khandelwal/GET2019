6926e298b8089f674b3d66f3fba4bcac166d0db1
meta-akshat-khandelwal <akshat.khandelwal@meatcube.com>
2019-03-16 18:26:36 +0530

    EAD Assignment

---
+++ Angular/EADAssignment9/src/app/card/card.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import {users} from '../mockUsers' ;
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+//getting the users list from the mockUsers.ts file
+array_users = users;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ Angular/EADAssignment9/src/app/mockUsers.ts
@@ -0,0 +1,10 @@
+import { user } from './users';
+
+export const users: user[]=[
+    {name: "Akshat", contact: 7788994455, city:"Jaipur"},
+    {name: "Shubham", contact: 7788994455, city:"Mumbai"},
+    {name: "Rahul", contact: 7788994455, city:"Delhi"},
+    {name: "Nitesh", contact: 7788994455, city:"Jaipur"},
+    {name: "Joy", contact: 7788994455, city:"Ahmedabad"},
+    {name: "Jaya", contact: 7788994455, city:"Kolkata"},
+];
